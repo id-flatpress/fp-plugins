eff5126f0642745c4aa9ad13c9727902e71c0f54
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:47:32 +0700

    Create lang.id-id.php

---
+++ mobile/lang/lang.id-id.php
@@ -0,0 +1,18 @@
+    <?php
+     
+    $lang['admin']['plugin']['submenu']['mobile'] = 'Mobile';
+    $lang['admin']['plugin']['mobile'] = array(
+            'head' => 'Konfigurasi Mobile',
+            'desc1' => 'Plugin ini memungkinkan anda untuk menggunakan tema yang berbeda untuk perangkat mobile.',
+     
+            'theme' => 'Tema',
+            'removejs' => 'Buang Javascript',
+            'admin' => 'Gunakan juga di Admin Area',
+     
+            'submit' => 'Simpan Konfig',
+            'msgs' => array(
+                    1 => 'Konfigurasi disimpan.',
+                    -1 => 'Ada kesalahan saat mencoba untuk menyimpan konfigurasi.',
+                    -2 => 'Tema yang anda pilih tidak ada.',
+            )
+    );
