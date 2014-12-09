66af07c690f93773a9d8cd227ba394d276fff06c
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:46:49 +0700

    Create lang.id-id.php

---
+++ redirect/lang/lang.id-id.php
@@ -0,0 +1,43 @@
+<?php
+
+$lang['admin']['plugin']['submenu']['redirect'] = 'Redirect';
+$lang['admin']['plugin']['redirect'] = array(
+	'head' => 'Konfigurasi Redirec',
+	'desc1' => 'Plugin ini memungkinkan untuk membuat pengalihan dari URL yang berbeda untuk entri.',
+
+	'name' => 'Nama Redirect',
+	'goto' => 'Destinasi Redirect (entri id)',
+	'opts' => 'Opsi lain',
+	'edit' => 'Rubah',
+	'delete' => 'Hapus',
+
+	'inccomm' => 'Sertakan komentar',
+	'httpredirect' => 'Lakukan Pengalihan HTTP',
+
+	'noredirect' => 'There are no redirects yet.',
+	'newredirect' => 'Create a new redirect:',
+
+	'delconfirm1' => 'You\'re going to delete redirect ',
+	'delconfirm2' => '. Are you sure you want to proceed?',
+	'yes' => 'Yes, delete this redirect',
+	'no' => 'No, take me back to the panel',
+	'or' => 'or',
+
+	'submit' => 'Save redirects',
+	'cancel' => 'Cancel',
+
+	'msgs' => array(
+		// New or edit
+		1 => 'Pengalihan disimpan.',
+		-1 => 'Terjadi kesalahan saat mencoba menyelamatkan pengalihan.',
+		-2 => 'Redirect Anda mencoba untuk mengedit tidak ada.',
+		-5 => 'Masuknya tujuan tidak ada.',
+		-6 => 'Nama redirect tidak valid!',
+
+		// Delete
+		-3 => 'Redirect yang Anda coba hapus tidak ada.',
+		4 => 'Redirect terhapus.',
+		-4 => 'Terjadi kesalahan saat mencoba untuk menghapus redirect.'
+	)
+);
+
