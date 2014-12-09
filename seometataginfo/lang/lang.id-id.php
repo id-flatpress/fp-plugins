44a11003a9b83a4387d71a6578173ce6cf2f5897
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:41:56 +0700

    Create lang.id-id.php

---
+++ seometataginfo/lang/lang.id-id.php
@@ -0,0 +1,26 @@
+<?php
+
+$lang['admin']['plugin']['seometataginfo'] = array (
+	'legend_desc'		=> 'Meta tag',
+	'input_desc'		=> 'Masukkan diskripsi pada entri ini:',
+	'input_keywords'	=> 'Masukkan keywords pada entri ini:',
+	'input_noindex'		=> 'Disallow Indexing:',
+	'input_nofollow'	=> 'Disallow Following:',
+	'input_noarchive'	=> 'Disallow Archiving:',
+	'input_nosnippet'	=> 'Disallow snippets:',
+);
+
+$lang['plugin']['seometataginfo'] = array (
+	'sep'		=> ' - ',
+	'home'		=> 'Beranda',
+	'blog_home' => 'Blog',
+	'blog_page' => 'Blog',
+	'archive'	=> 'Archive',
+	'category'	=> 'Kategori',
+	'tag'		=> 'Tag',
+	'contact'	=> 'Kontak',
+	'comments'	=> 'Komentar',
+	'pagenum'	=> 'Halaman #'
+);
+
+?>
