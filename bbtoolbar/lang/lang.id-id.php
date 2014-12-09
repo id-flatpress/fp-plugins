38e63113758fc1a375d885c23e0f999ae75aceb2
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:49:36 +0700

    Create lang.id-id.php

---
+++ bbtoolbar/lang/lang.id-id.php
@@ -0,0 +1,61 @@
+<?php
+$lang['plugin']['bbtoolbar']=array(
+	'bold'=>'Tebal',
+	'italic'=>'Miring',
+	'underline'=>'Garis Bawah',
+	'del'=>'Garis Tengah',
+	'quote'=>'Kutipan',
+	'code'=>'Kode',
+	'tag'=>'Sisipkan tag',
+
+	'image'=>'Sisipkan Gambar',
+	'imgsource'=>'Sumber',
+	'imgurl'=>'Sisipkan alamat URL Gambar: ',
+	'imglist'=>'Atau pilih dari daftar: ',
+	'imgoptions'=>'Pilihan',
+	'imgboxt'=>'Sisipkan Gambar',
+	'imgfloat'=>'Float (opsional): ',
+	'none'=>'None',
+	'left'=>'Kiri',
+	'right'=>'Kanan',
+	'imgscale'=>'Skala (opsional): ',
+	'imgwidth'=>'Lebar (opsional): ',
+	'imgheight'=>'Height (opsional): ',
+	'imgpopup'=>'Popup? ',
+	
+	'url'=>'Sisipkan link atau lampiran',
+	'urlboxt'=>'Sisipkan URL',
+	'urlboxinsert'=>'Sisipkan alamat:',
+	'urlboxatt'=>'Atau pilih lampiran:',
+	'urlbotnoatt'=>'Tidak ada lampiran ',
+
+	'boxok'=>'Oke',
+	'boxcancel'=>'Batal',
+);
+
+$lang['admin']['plugin']['submenu']['bbtoolbar']='BBToolbar';
+$lang['admin']['plugin']['bbtoolbar']=array(
+	'head'=>'Konfiugurasi BBToolbar',
+	'desc1'=>'Plugin ini memungkinkan untuk memiliki toolbar BBCode lebih baik dari yang standar.',
+
+	'toolbar'=>'Toolbar',
+	'en_en'=>'Tampilkan dalam editor entri',
+	'en_co'=>'Tampilkan pada komentar',
+	'tcyes'=>'Ya',
+	'tcno'=>'Tidak',
+	'tcauto'=>'Hanya jika <a href="./admin.php?p=plugin&action=bbcode">BBCode diaktifkan untuk komentar</a> ',
+	'tab'=>'Tambahkan pengolahan ketika Anda menekan tombol Tab ',
+
+	'emoticons'=>'Emotikon',
+	'em_en_p'=>'Ganti Emotikon pada Tulisan Entri',
+	'em_co_p'=>'Ganti Emotikon pada Komentar',
+	'em_set'=>'Set apa yang ingin Anda gunakan? ',
+	'em_en_tb'=>'Tambahkan toolbar emotikon ke entri editor',
+	'em_co_tb'=>'Tambahkan toolbar emotikon untuk komentar editor',
+
+	'submit'=>'Simpan Konfigurasi',
+	'msgs'=>array(
+		1=>'Konfigurasi disimpan. ',
+		-1=>'Terjadi kesalahan saat mencoba untuk menyimpan konfigurasi.',
+	)
+);
