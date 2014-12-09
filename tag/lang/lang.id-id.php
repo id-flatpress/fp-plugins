bc4690acfd8334e208523d79e3a40902041b623e
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:40:52 +0700

    Create lang.id-id.php

---
+++ tag/lang/lang.id-id.php
@@ -0,0 +1,22 @@
+<?php
+	$lang['plugin']['tag']=array(
+		'tag_s'=>'Tag(s): ',
+		'title_pre'=>'Tag: ',
+
+		'widget'=>'Tag Cloud',
+		'oneentry' => 'Satu entri',
+		'entry'=>' entri',
+		'entries'=>' Tulisan',
+		'notags'=>'Tidak ada tag apapun.',
+
+		'related'=>'Tulisan Terkait',
+		'norelated'=>'Tidak menemukan entri terkait.',
+		'error'=>'Kesalahan',
+	);
+	$lang['plugin']['tag']['errors']=array (
+		-1	=> 'Plugin ini membutuhkan BBCode untuk dimuat. Mungkin plugin telah dimuat dalam urutan yang salah'
+	);
+	$lang['admin']['plugin']['tag']=array(
+		'tag_pl'=>'Plugin Tag',
+		'remove'=>'Buang',
+	);
