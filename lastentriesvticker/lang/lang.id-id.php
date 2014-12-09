3dc43f56e89a4771258ffd9eaa1a54f62c6f42a8
Baguz A <eggoez@users.noreply.github.com>
2014-12-09 11:48:55 +0700

    Create lang.id-id.php

---
+++ lastentriesvticker/lang/lang.id-id.php
@@ -0,0 +1,15 @@
+<?php
+
+	$lang['plugin']['lastentriesvticker'] = array(
+		
+		'latest'  => 'Tulisan',
+    'subject_before_count'	=> 'Baru ',
+		'subject_after_count'	=> ' Terakhir',
+		'edit'			=> 'Rubah',
+		'add_entry'		=> 'Tambah entri',
+		'no_entries'	=> 'Tidak ada entri'
+	
+	
+	);
+
+?>
