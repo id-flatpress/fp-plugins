4efb63ca4386862612fb6fff041dbe09e0abbe69
Baguz A <eggoez@users.noreply.github.com>
2014-12-13 11:11:28 +0700

    Create lang.id-id.php

---
+++ title/lang/lang.id-id.php
@@ -0,0 +1,28 @@
+<?php
+$lang['admin']['plugin']['submenu']['titles']='Titles';
+$lang['admin']['plugin']['titles']=array(
+	'head'=>'Konfigurasi Titles',
+	'desc1'=>'Plugin ini memungkinkan untuk menggunakan HTML disesuaikan <code>Judul</code> tag.',
+	'replace'=>'Anda bisa menggunakan <code>[title]</code> untuk judul standar dan'.
+	'<code>[sep]</code> untuk char pemisah yang disediakan oleh tema.',
+	'cat'=>'Entri dengan daftar kategori',
+	'catd'=>'[category] berarti nama kategori',
+	'tag'=>'Entri dengan daftar tag',
+	'tagd'=>'[tag] berarti nama tag',
+	'page'=>'Halaman Sebelumnya',
+	'paged'=>'[page] berarti nomor halaman',
+	'error404'=>'Error 404',
+	'archive'=>'Arsip',
+	'date'=>'Di sini Anda juga dapat menggunakan PHP\'s <a href="http://www.php.net/strftime">strftime</a>'.
+	' meta karakter.',
+	'y'=>'Arsip tahunan',
+	'm'=>'Arsip bulanan',
+	'd'=>'Arsip harian',
+
+	'submit'=>'Simpan konfigurasi',
+	
+	'msgs'=>array(
+		1=>'Konfigurasi disimpan.',
+		-1=>'Terjadi kesalahan saat mencoba untuk menyimpan konfigurasi.',
+	)
+);
