feabd5d60769141c1054e1894e4cb288c0555a01
Baguz A <eggoez@users.noreply.github.com>
2014-12-13 11:03:42 +0700

    Create lang.id-id.php

---
+++ fpstats/lang/lang.id-id.php
@@ -0,0 +1,55 @@
+<?php
+
+    $lang['admin']['plugin']['submenu']['fpstats'] = 'FPStats';
+
+    $lang['admin']['plugin']['fpstats'] = array(
+
+            'head'		=> 'FPStats',
+
+            'description'=>'Plugin ini memberikan beberapa statistik dasar tentang pengunjung Anda.',
+
+            'visitedPages'  => 'Halaman dikunjungi',
+
+            'filter'	=> 'Saring',
+
+            'from'      => 'Dari',
+
+            'to'        => 'Ke',
+
+            'overview'  => 'Ikhtisar',
+
+            'rawdata'   => 'Data raw',
+
+            'noRecords' => 'Tidak ada',
+
+            'date'      => 'Tanggal',
+
+            'allHits'   => 'Semua hits',
+
+            'uniqueHits'=> 'Hits unik',
+
+            'dayStats'  => 'Statistik hari',
+
+            'weekStats' => 'Statistik minggu',
+
+            'monthStats'=> 'Statistik bulan',
+
+            'yearStats' => 'Statistik tahun',
+
+            'crawlers'  => 'Crawlers',
+
+            'purgeCache'=> 'Bersihkan cache',
+
+            'confirmCacheCleaning' => 'Cache penyegaran cukup lama. Apakah Anda yakin?',
+
+            'browsersAndOses'    => 'Browser & systems operasi',
+
+            'browsersAndOsesShort'  => 'Browser & OSes',
+
+            'since' => 'sejak'
+
+    );
+
+
+
+?>
