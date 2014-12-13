60e2bc80536414a3f27875ff4a192a230c0e7c84
Baguz A <eggoez@users.noreply.github.com>
2014-12-13 10:56:01 +0700

    Create lang.id-id.php

---
+++ fpcaptcha/lang/lang.id-id.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+	$lang['plugin']['fpcaptcha'] = array(
+
+		
+
+		'prefix'	=> 'Untuk membantu menghilangkan serangan spam otomatis '.
+
+				'silahkan masukkan kode di bawah ini. '.
+
+				'<br /><br />',
+
+		
+
+		'error'		=> 'Maaf, Anda memasukkan kode yang salah. Harap coba lagi.'
+
+	
+
+	);
+
+
+
+?>
+
